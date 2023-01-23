3bd8f8f54cf6ac6c4b9521126965928187355681
KiLcRaFt <killcraft96@gmail.com>
2023-01-23 12:54:52 +0200

    Done

--- Контрольная работа по теме Циклы/Контрольная_работа_по_теме_Циклы.py
+++ Контрольная работа по теме Циклы/Контрольная_работа_по_теме_Циклы.py
@@ -1,19 +1,19 @@
 ﻿from random import *
 
 #Ulesanne 1
-#try:
-#    n = int(input("Введите число зверьков от 1 - 9: "))
-#    a = 1
-#    while n<=9 and n>0 and a<=n:
-#        for x in range(n):
-#            print("\n ^---^", end=" \n")
-#            print("( o o )", end="  \n")
-#            print(" ! = !/)", end=" \n") 
-#            a+=1
-#    else:
-#        print("Введите число от 1 - 9")
-#except:
-#    print(TypeError)
+try:
+    n = int(input("Введите число зверьков от 1 - 9: "))
+    a = 1
+    while n<=9 and n>0 and a<=n:
+        for x in range(n):
+            print("\n ^---^", end=" \n")
+            print("( o o )", end="  \n")
+            print(" ! = !/)", end=" \n") 
+            a+=1
+    else:
+        print("Введите число от 1 - 9")
+except:
+    print(TypeError)
 
 #Ülesanne 2
 
@@ -31,9 +31,10 @@
 
 #Ülesanne 3
 
-opilased = randint(10, 42)
-a = []
-for i in range(10, opilased):
-    hinne = randint(1, 100)
-    print(hinne)
-    print (a.append(hinne))
+#opilased = randint(10, 42)
+#a = []
+#for i in range(10, opilased):
+#    hinne = randint(1, 100)
+#    a.append(hinne)
+#print(f"Самое минимальное колличество ", min(a), "баллов")
+#print(f"Самое максимальное колличество ", max(a), "ебаллов")
