df20138fbdc4beda6ed772248b39e58e66db12ea
opilane <opilane@HP-CZC2349HT2>
2023-02-08 15:47:22 +0200

    o2

--- Palgad/Palgad/OmaMoodul.py
+++ Palgad/Palgad/OmaMoodul.py
@@ -33,8 +33,79 @@ def Kustutamine(i:list, p:list):
 
 def suurim_palg(i:list, p:list):
     """
-    Küsib inimese list ja palgad list
+    Küsib suurim palg
     :param list i:inimeste järjend
     :param list p: palgade järjend
     :rtype list, list:s
     """
+    # ise kirjutada, kui mitu palgad
+    palk=max(p)
+    ind=p.index(palk)
+    nimi=i[ind]
+    return palk, nimi
+
+def väiksem_palg(i:list, p:list):
+    """
+    Küsib suurim palg
+    :param list i:inimeste järjend
+    :param list p: palgade järjend
+    :rtype list, list:s
+    """
+    # ise kirjutada, kui mitu palgad
+    palk=min(p)
+    ind=p.index(palk)
+    nimi=i[ind]
+    return palk, nimi
+
+def Sorteerimine(i:list, p:list):
+    """
+    Küsib suurim palg
+    :param list i:inimeste järjend
+    :param list p: palgade järjend
+    :rtype list, list:s
+    """
+    # ise kirjutada, kui mitu palgad
+    v=int(input("1-kahaneb, 2-kasvab"))
+    if v == 1:
+        n=len(p)
+        for j in range(0, n-1):
+             for k in range(j+1, n):
+                 if p[j]<p[k]:
+                     abi=p[j]
+                     p[j]=p[k]
+                     p[k]=abi
+                     abi=i[j]
+                     i[j]=i[k]
+                     i[k]=abi
+    elif v == 2:
+        n=len(p)
+        for j in range(0, n-1):
+             for k in range(j+1, n):
+                 if p[j]>p[k]:
+                     abi=p[j]
+                     p[j]=p[k]
+                     p[k]=abi
+                     abi=i[j]
+                     i[j]=i[k]
+                     i[k]=abi
+
+    return i, p
+
+def Vordsed_palgad(i:list, p:list):
+    """
+    Küsib suurim palg
+    :param list i:inimeste järjend
+    :param list p: palgade järjend
+    :rtype list, list:s
+    """
+    dublikatid=[x for x in p if p.count(x)>1 ]
+    dublikatid=list(set(dublikatid)) #[1200, 2500, 750, 750, 1200] -> [1200, 750]
+    for palk in dublikatid:
+        n=p.count(palk)
+        k=-1
+        print(f"{palk} saavad kätte järgmised inimesed:")
+        for j in range(n):
+            k=p.index(palk,k+1)
+            nimi=i[k]
+            print(nimi, "saab kätte", palk)
+
--- Palgad/Palgad/Palgad.py
+++ Palgad/Palgad/Palgad.py
@@ -1,13 +1,21 @@
 ﻿from OmaMoodul import *
 palgad=[1200,2500,750,395,1200]
-inimesed=["A","B","C","D","E"]
+inimesed=["Arhagel","Biba","Coop","Delay","Eeeesports"]
 while True:
     print(inimesed)
     print(palgad)
-    menu=int(input("Valik:`\n 1-Lisa andmed\n2-Kustuta andmed"))
+    menu=int(input("Valik:\n 1-Lisa andmed\n 2-Kustuta andmed\n 3-Suurim palg\n 4-Väiksem andmed\n 5-Sorteerimine\n 6-Vordsed palgad\n"))
     if menu==0:
         break
     elif menu==1:
         inimesed, palgad=Lisa_andmed(inimesed, palgad)
     elif menu==2:
         inimesed, palgad=Kustutamine(inimesed, palgad)
+    elif menu==3:
+        inimesed, palgad=suurim_palg(inimesed, palgad)
+    elif menu==4:
+        inimesed, palgad=väiksem_palg(inimesed, palgad)
+    elif menu==5:
+        inimesed, palgad=Sorteerimine(inimesed, palgad)
+    elif menu==6:
+        inimesed, palgad=Vordsed_palgad(inimesed, palgad)
