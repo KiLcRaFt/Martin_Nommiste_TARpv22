39507e7bc42ae82929e7130fddf8b210ad1eff45
opilane <opilane@HP-CZC2349HT2>
2023-02-08 14:22:23 +0200

    o

---
+++ Palgad/Palgad/OmaMoodul.py
@@ -0,0 +1,40 @@
+﻿from datetime import *
+#8/02/23
+def Lisa_andmed(i:list, p:list):
+    """
+    Küsib inimese list ja palgad list
+    :param list i:inimeste järjend
+    :param list p: palgade järjend
+    :rtype list, list:
+    """
+    n=int(input("Mitu inimest: "))
+    for j in range(n):
+        nimi=input("Sisesta nimi: ")
+        palk= int(input("Sisesta palk: "))
+        i.append(nimi)
+        p.append(palk)
+    return i, p
+
+def Kustutamine(i:list, p:list):
+    """
+    Küsib inimese list ja palgad list
+    :param list i:inimeste järjend
+    :param list p: palgade järjend
+    :rtype list, list:s
+    """
+    nimi=input("Sisesta nimi: ")
+    if nimi in i:
+        n=i.count(nimi)
+        for j in range(n):
+            ind=i.index(nimi)
+            i.pop(ind)
+            p.pop(ind)
+    return i, p
+
+def suurim_palg(i:list, p:list):
+    """
+    Küsib inimese list ja palgad list
+    :param list i:inimeste järjend
+    :param list p: palgade järjend
+    :rtype list, list:s
+    """
---
+++ Palgad/Palgad/Palgad.py
@@ -0,0 +1,13 @@
+﻿from OmaMoodul import *
+palgad=[1200,2500,750,395,1200]
+inimesed=["A","B","C","D","E"]
+while True:
+    print(inimesed)
+    print(palgad)
+    menu=int(input("Valik:`\n 1-Lisa andmed\n2-Kustuta andmed"))
+    if menu==0:
+        break
+    elif menu==1:
+        inimesed, palgad=Lisa_andmed(inimesed, palgad)
+    elif menu==2:
+        inimesed, palgad=Kustutamine(inimesed, palgad)
