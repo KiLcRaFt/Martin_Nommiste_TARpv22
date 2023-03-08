2ec966626cec01f3843849c1c67912eb9968d71c
opilane <opilane@HP-CZC2349HT0>
2023-03-08 14:52:03 +0200

    done

--- Sonstik/Sonstik/OmaMoodul.py
+++ Sonstik/Sonstik/OmaMoodul.py
@@ -21,13 +21,13 @@ def salvesta_failisse(f,text):
     mas=loe_failist(f)
     return mas
 def uued_sonad():
-    rus_sona=input("Введи слово на русском:")
+    rus_sona=input("Sisesta sõna vene keeles:")
     est_sona=input("Sisesta sõna eesti keeles:")
     rus_list=salvesta_failisse("rus.txt",rus_sona)
     est_list=salvesta_failisse("est.txt",est_sona)
     return rus_list, est_list
 def tolkimine(rus_list,est_list):
-    slovo=input("Введите слово для перевода: ")
+    slovo=input("Sisestage tõlgitav sõna: ")
     if slovo in rus_list:
         ind=rus_list.index(slovo)
         print(f"{slovo} - {est_list[ind]}")
@@ -35,14 +35,14 @@ def tolkimine(rus_list,est_list):
         ind=est_list.index(slovo)
         print(f"{slovo} - {rus_list[ind]}")
     else:
-        print(f"{slovo.upper()} отсутствует в словаре")
-        v=input("Желаете добавить слово в словарь?")
-        if v.lower()=="да": uued_sonad()
+        print(f"{slovo.upper()} pole sõnastikus")
+        v=input("Kas soovite sõnaraamatusse sõna lisada??")
+        if v.lower()=="jah": uued_sonad()
 def parandus(rus_list,est_list):
-    viga=input("Введите слово с ошибкой: ")
+    viga=input("Sisestage valesti kirjutatud sõna: ")
     if viga in rus_list:
         ind=rus_list.index(viga)
-        print(f"Будет исправлена пара слов{viga} - {est_list[ind]}")
+        print(f"Paar sõna parandatakse{viga} - {est_list[ind]}")
         rus_list.pop(ind)
         est_list.pop(ind)
         rus_list=list_failisse("rus.txt",rus_list)
@@ -51,14 +51,14 @@ def parandus(rus_list,est_list):
         
     elif viga in est_list:
         ind=est_list.index(viga)
-        print(f"Будет исправлена пара слов{viga} - {rus_list[ind]}")
+        print(f"Paar sõna parandatakse{viga} - {rus_list[ind]}")
         rus_list.pop(ind)
         est_list.pop(ind)
         rus_list=list_failisse("rus.txt",rus_list)
         est_list=list_failisse("est.txt",est_list)
         uued_sonad()
     else:
-        print(f"{viga.upper()} отсутствует в словаре")
+        print(f"{viga.upper()} pole sõnastikus")
     rus_list=loe_failist("rus.txt")
     est_list=loe_failist("est.txt")
     return rus_list,est_list
@@ -67,37 +67,37 @@ def game(rus_list,est_list):
     x=[]
     v=k=0
     rus,est=(rus_list,est_list)
-    numb=input("Сколько игр вы хотите сыграть? ")
-    for i in range(int(numb)):
+    numb=int(input("Kui palju mänge soovite mängida? "))
+    for i in range(numb):
         num=randint(0,(len(rus)-1))
         while num in x:
             num=randint(0,(len(rus)-1))
         try:
-            keel=input("На каком языке вы хотите играть?\n1-русский\n2-эстонский ").lower()
+            keel=int(input("Mis keeles soovite mängida?\n1-vene\n2-eesti "))
         except:
             print("Value Error")
         if keel==1:                                               
-            tolk=input(f"Как переводиться {rus[num]} правильно? ") 
+            tolk=input(f"Kuidas tõlkida {rus[num]} õige? ") 
             if tolk==est[num]:
-                game.append(f"{i+1} {keel} ответ - правильный")
-                print("Правильно") 
+                print(f"{keel} vastus on õige")
+                print("Õige\n") 
                 v+=1
             else:
-                game.append(f"{i+1}, {keel} ответ - неправильный") 
-                print("Неправильно")
+                print(f"{keel} vastus on vale") 
+                print("Vale\n")
                 k+=1
         elif keel==2:                                                              
-            tolk=input(f"Как переводиться {est[num] } правильно? ") 
+            tolk=input(f"Kuidas tõlkida {est[num] } õige? ") 
             if tolk==rus[num]:
-                game.append(f"{i+1} {keel} ответ - правильный")
-                print("правильно")
+                print(f"{keel} vastus on õige")
+                print("Õige\n")
                 v+=1
             else:
-                game.append(f"{i+1}, {keel} ответ - неправильный") 
-                print("Неправильно")
+                print(f"{keel} vastus on vale") 
+                print("Vale\n")
                 k+=1
         x.append(num)
-        resV=round((v/num*100),1)
-        resK=round((k/num*100),1)
-        print(f"Процент выигрыша - {resV}%")
-        print(f"Процент потерь - {resK}%")
+        resV=v*100//numb
+        resK=k*100//numb
+    print(f"Õigete sõnade protsent - {resV}%")
+    print(f"Valede sõnade protsent - {resK}%")
