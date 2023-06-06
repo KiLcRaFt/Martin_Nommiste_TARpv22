9d6f2ec89e1d34d0650cf5a30838c98e2c23bef6
M0RGAN <M0RGAN@DESKTOP-P09LV9A>
2023-06-07 00:25:01 +0300

    done

---
+++ Kordamine_C#/Kordamine_C#/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] num = new int[1, 2, 3, 4, 5];
+
+        bool vozras = true;
+        bool ubivanie = true;
+
+        for (int i = 1; i < num.Length; i++)
+        {
+            if (num[i] < num[i - 1])
+            {
+                vozras = false;
+                break;
+            }
+        }
+
+        for (int i = 1; i < num.Length; i++)
+        {
+            if (num[i] > num[i - 1])
+            {
+                ubivanie = false;
+                break;
+            }
+        }
+
+        if (vozras or ubivanie)
+        {
+            Console.WriteLine("Массив упорядочен");
+        }
+        else
+        {
+            Console.WriteLine("Массив не упорядочен");
+        }
+    }
+}
